1b91164517a201cdb8080c92d417a61bdfbafd04
Tyler <tylerhbaker99@gmail.com>
2025-05-18 02:30:33 -0400

    add style and music library, mobile friendly

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,12 @@ class Navbar extends HTMLElement {
     super();
 
     this.innerHTML = `
-      <nav class="nav">
+      <nav>
 	<a href="index.html" title="Chaos Control Home">Home</a>
+	<a href="music-library.html" title="Music Library">Music Library</a>
+	<a href="#" title="#">Language Learning</a>
+	<a href="#" title="#">Blog</a>
+	<a href="#" title="#">Fediverse</a>
       </nav>
     `;
   }
