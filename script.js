dd8f82f33409bb20e1bc365c564e2a3130c41079
Tyler Baker <internetexplorerbad@robustjumprope.lol>
2025-07-22 15:42:16 -0400

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const $ = (cssQuery) => returnType(document.querySelectorAll(cssQuery));
+
+const returnType = (result) => {
+  switch (result.length) {
+    case 1:
+      return result[0];
+    case 0:
+      return undefined;
+    default:
+      return result;
+  }
+};
+
+const signal = (initialValue) => {
+  const observers = new Set();
+  const subscribe = (func) => observers.add(func);
+  const unsubscribe = (func) => observers.delete(func);
+  const notify = (data) => observers.forEach((observer) => observer(data));
+};
+
 const componentHolder = () => document.getElementById("holder");
 document.querySelectorAll(".navlink").forEach((link) => {
   link.addEventListener("click", (e) => {
